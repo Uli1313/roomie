1bc0459ecc8460d313d29365b29df2b2fe0ca860
Uli <ru21041555@gmail.com>
2023-11-10 17:20:03 +0800

    feat: 首頁 介紹區塊切版

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,16 @@
 import "swiper/css/bundle";
 import Swiper from "swiper/bundle";
+
+const swiperIntro = new Swiper(".intro-swiper", {
+  slidesPerView: "auto",
+  spaceBetween: 12,
+  breakpoints: {
+    768: {
+      spaceBetween: 24,
+    },
+  },
+});
+
 const swiperLatestRent = new Swiper(".latest-rent-swiper", {
   slidesPerView: 1,
   spaceBetween: 12,
@@ -25,3 +36,29 @@ const swiperLatestRent = new Swiper(".latest-rent-swiper", {
     },
   },
 });
+
+// 打字機
+const arr = ["工作室", "住處"];
+let count = 0;
+setInterval(function () {
+  count += 1;
+  const typewriteAni = document.getElementById("typewrite-animated");
+  const typewriteText = document.getElementById("typewrite-text");
+  if (typewriteAni.classList.contains("typewriter")) {
+    typewriteAni.classList.remove("typewriter", "typingtwo", "typingthree");
+  } else {
+    typewriteAni.classList.add("typewriter");
+    if (count % 2 === 1) {
+      typewriteAni.classList.add("typingtwo");
+    } else {
+      typewriteAni.classList.add("typingthree");
+    }
+    typewriteAni.addEventListener("animationend", function () {
+      setTimeout(function () {
+        typewriteText.textContent = "";
+        typewriteAni.classList.remove("typewriter", "typingtwo", "typingthree");
+      }, 800);
+    });
+  }
+  typewriteText.textContent = arr[count % 2];
+}, 1500);
