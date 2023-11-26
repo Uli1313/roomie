8124e65c4eff524e4414511434be8840e2aac5dd
Uli <ru21041555@gmail.com>
2023-11-26 20:03:05 +0800

    feat: 修改 render 路徑

--- assets/js/news.js
+++ assets/js/news.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-const url = "http://localhost:3000";
+// const url = "http://localhost:3000";
+const url = "https://roomie-nnwq.onrender.com";
 
 let newsData;
 let currentPage = 1;
