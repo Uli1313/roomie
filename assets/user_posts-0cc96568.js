7ae59e542eba5a77c69091583523c4964570c337
Uli <ru21041555@gmail.com>
2023-11-26 21:50:40 +0800

    Updates

--- assets/user_posts-0cc96568.js
+++
@@ -1,1 +0,0 @@
-import"./main-de4656de.js";const o=[].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));o.map(function(t){return new bootstrap.Tooltip(t)});
---
+++ assets/user_posts-ea1c7a5b.js
@@ -0,0 +1,1 @@
+import"./bootstrap.min-78670b47.js";const o=[].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));o.map(function(t){return new bootstrap.Tooltip(t)});
