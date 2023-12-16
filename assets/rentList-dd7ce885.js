b6332af63e2137f777da40100d517f5f48282ad4
Peng <linken0508@gmail.com>
2023-12-16 16:16:57 +0800

    Updates

---
+++ assets/rentList-dd7ce885.js
@@ -0,0 +1,62 @@
+import{a as d,S}from"./bootstrap.min-ee9b3e4d.js";const H="https://roomie-lfta.onrender.com",O=localStorage.getItem("userId"),z=localStorage.getItem("token"),V={headers:{Authorization:`Bearer ${z}`}};let N;const U=document.querySelectorAll(".nav-logged"),B=document.querySelectorAll(".nav-unlogged"),Y=document.querySelectorAll(".nav-logged-photo"),J=document.querySelector(".logout-btn");(async()=>{try{const s=`/600/users/${O}`,a=`${H}${s}`,c=await d.get(a,V),i=c.data.photo;Y.forEach(o=>o.setAttribute("src",i)),N=c.data,W(),U.forEach(o=>{o.classList.remove("d-none")}),B.forEach(o=>{o.classList.add("d-none")})}catch(s){console.log(s),U.forEach(a=>{a.classList.add("d-none")}),B.forEach(a=>{a.classList.remove("d-none")})}})();function W(){localStorage.setItem("user",JSON.stringify(N))}J.addEventListener("click",G);function G(){S.fire({icon:"question",title:"確定要登出嗎",showCancelButton:!0,confirmButtonText:"確定",cancelButtonText:"取消"}).then(s=>{s.isConfirmed&&(localStorage.removeItem("userId"),localStorage.removeItem("token"),localStorage.removeItem("user"),S.fire({icon:"success",title:"已登出",timer:1500,showConfirmButton:!1}),setTimeout(()=>{location.href="login.html"},1e3))})}const f="https://roomie-lfta.onrender.com/";let t="",g=[];function K(s){let a=document.querySelector("#list"),c="";s.forEach(function(i){let o=i.canPet?"可養寵物":"不可養寵物",b=i.canCooking?"可開伙":"不可開伙",p="";i.traffic.forEach(function(x){p+=`<span class="me-3 px-1 bg-primary-200">近${x}</span>`}),c+=`<div class="row p-1 my-4 rounded hover-primary-2 ">
+                    <div class="col-12 col-md-3 p-0 rounded-top rounded-md-start">
+                        <img 
+                            src="${i.photo[0]}" 
+                            style="width:auto;height:100%;object-fit: cover;object-position: center;" 
+                            alt="house photo"
+                            class="rounded-top rounded-md-start">
+                    </div>
+                    <div class="col-12 col-md-9 py-3 px-5 border border-start-sm-0 bg-white rounded-bottom rounded-md-end">
+                        <ul>
+                            <li class="w-100 d-flex justify-content-between align-items-center py-2">
+                                <a href="rentArticle.html?id=${i.id}" class="h3 link-dark link-title">${i.title}</a> 
+                                <button class="p-3 link-dark hover-primary border-0 rounded-3 favorite" data-id="${i.id}">
+                                    <span class="material-symbols-outlined" data-id="${i.id}">heart_plus</span>
+                                </button></li>
+                            <li class="pb-3">${i.houseLayout} | ${i["square Footage"]}坪 | ${i.floor}F/${i.totalFloor}F </li>
+                            <li class="pb-2">
+                                <span class="material-symbols-outlined pe-2 transform-y-25">location_on</span>
+                                ${i.address} ${i.district[0]}-${i.district[1]}</li>
+                            <li class="pb-3">
+                                <span class="material-symbols-outlined pe-1 transform-y-25">person</span>
+                                <span class="me-3 px-1 bg-primary-200">${i.gender}</span>
+                                <span class="me-3 px-1 bg-primary-200">${o}</span>
+                                <span class="me-3 px-1 bg-primary-200">${b}</span></li>
+                            <li class="pb-2">
+                                <span class="material-symbols-outlined pe-1 transform-y-25">map</span>
+                                ${p}</li>
+                            <li class="pb-2 h2 text-danger text-end">${i.price.toLocaleString("zh-TW")}元/月</li>
+                            <li class="d-flex justify-content-between">更新日期:${i.updateDate} 
+                                <div>
+                                    <span class="material-symbols-outlined transform-y-25">visibility</span>
+                                    <span class="ps-2">${i.view}</span>
+                                </div></li>
+                        </ul>
+                    </div>
+                </div>`}),a.innerHTML=c,se(),re()}function _(){let s=document.querySelector("#list"),a=`
+                <div class="row rounded">
+                    <div class="col bg-white rounded m-5 p-5 d-flex flex-column justify-content-center align-items-center ">
+                        <p>¯_(ツ)_/¯</p>
+                        <p class="p-2 h5">對不起，沒有找到適合您的物件！</p>
+                        <p>建議您：重新篩選或搜尋試試看唷～</p>
+                    </div>
+                </div>`;s.innerHTML=a}function Q(){t="_sort=view&_order=desc&status=刊登中",d.get(`${f}rents?${t}`).then(function(s){g=s.data,$(t,r,h),m(g)})}Q();let X=document.querySelector("#filterDate"),Z=document.querySelector("#filterPrice"),y="&_sort=updateDate&_order=asc",C="&_sort=updateDate&_order=desc",k="&_sort=price&_order=asc",w="&_sort=price&_order=desc",I="_sort=view&_order=desc";function ee(){X.addEventListener("click",function(s){t.includes(y)?t=t.replace(y,C):t.includes(C)?t=t.replace(C,y):t.includes(k)?t=t.replace(k,y):t.includes(w)?t=t.replace(w,y):t.includes(I)?t=t.replace(I,y):t+=y,d.get(`${f}rents?${t}`).then(function(a){g=a.data,g.length>0?($(t,1,h),r=1,m(g)):(_(),r=1,m(g))})})}ee();function te(){Z.addEventListener("click",function(s){t.includes(k)?t=t.replace(k,w):t.includes(w)?t=t.replace(w,k):t.includes(y)?t=t.replace(y,k):t.includes(C)?t=t.replace(C,k):t.includes(I)?t=t.replace(I,k):t+=k,d.get(`${f}rents?${t}`).then(function(a){g=a.data,g.length>0?($(t,1,h),r=1,m(g)):(_(),r=1,m(g))})})}te();let T,P,h=4,r=1,R=document.querySelector(".pagination");function $(s,a,c){d.get(`${f}rents?${s}&_page=${a}&_limit=${c}`).then(i=>{let o=i.data;K(o)})}function m(s){T=s.length,P=Math.ceil(T/h);let a="";if(T==0)a="";else{a+=`<li class="page-item">
+        <a class="page-link" href="#" aria-label="Previous">
+            <span class="material-icons align-bottom">
+            chevron_left
+            </span>
+        </a>
+        </li>`;for(let i=1;i<=P;i++)a+=`<li class="page-item">
+            <a class="page-link" href="#">${i}</a>
+        </li>`;a+=`<li class="page-item">
+        <a class="page-link" href="#" aria-label="Next">
+            <span class="material-icons"> navigate_next </span>
+        </a>
+        </li>`}R.innerHTML=a;const c=document.querySelectorAll(".page-link");c.forEach(i=>{(i.textContent.trim()==="chevron_left"||i.textContent.trim()==="navigate_next")&&i.blur()}),c[r].classList.add("active")}R.addEventListener("click",s=>{const a=document.querySelectorAll(".page-link");if(a.forEach(c=>{c.classList.remove("active")}),s.target.textContent.trim()==="chevron_left"){if(r===1){a[r].classList.add("active"),s.preventDefault();return}r>1&&(r--,a[r].classList.add("active"),$(t,r,h))}else if(s.target.textContent.trim()==="navigate_next"){if(r===P){a[r].classList.add("active"),s.preventDefault();return}r<P&&(r++,a[r].classList.add("active"),$(t,r,h))}else r=Number(s.target.textContent.trim()),a[r].classList.add("active"),$(t,r,h)});let ae=document.querySelector("#search"),ie=document.querySelector("#submit");ie.addEventListener("click",function(s){let c=`q=${ae.value}&status=刊登中`;d.get(`${f}rents?${c}`).then(function(i){let o=i.data;o.length>0?($(c,r,h),m(o)):_()})});d.get("https://gist.githubusercontent.com/abc873693/2804e64324eaaf26515281710e1792df/raw/a1e1fc17d04b47c564bbd9dba0d59a6a325ec7c1/taiwan_districts.json").then(function(s){let a=s.data,i=a.map(function(e){return{name:e.name,districts:e.districts.map(function(L){return L.name})}}).filter(e=>e.name!=="釣魚臺"&&e.name!=="南海島");function o(e,L){const l=["臺北市","新北市","基隆市","桃園市","新竹市","新竹縣","臺中市","彰化縣","苗栗縣","南投縣","雲林縣","高雄市","臺南市","嘉義市","嘉義縣","屏東縣","宜蘭縣","花蓮縣","臺東縣","澎湖縣","金門縣","連江縣"];return l.indexOf(e.name)-l.indexOf(L.name)}i.sort(o);let b=document.querySelectorAll(".countyCity"),p=document.querySelector(".btnTitle"),v=document.querySelector(".dropdownTitle"),x=document.querySelector(".districtList");document.querySelectorAll(".form-check-input"),b.forEach(function(e){e.addEventListener("change",function(L){document.querySelector("#disabled-events").classList.remove("disabled-events");let l=Array.from(b).indexOf(e),u=i[l],j=`${u.name}`,F=`${u.name}`,A="";u.districts.forEach(function(M,D){A+=` 
+                                        <li class="pb-2">
+                                            <div class="form-check-inline cursor">
+                                                <input class="form-check-input cursor districtCheckbox" type="checkbox" id="district${D}" value="${u.name}">
+                                                <label class="form-check-label cursor districtLabelText" for="district${D}">${M}</label>
+                                            </div>
+                                        </li>
+                                        `}),p.textContent=j,v.textContent=F,x.innerHTML=A,document.querySelectorAll(".form-check-input")})});let q=document.querySelector("#row"),n="",E="";q.addEventListener("change",function(e){const l=document.querySelector(`label[for='${e.target.id}']`).textContent.trim();e.target.checked?(e.target.type==="radio"&&e.target.classList.contains("cityRadio")?n=`address_like=${l}`:e.target.type==="checkbox"&&e.target.classList.contains("districtCheckbox")?n+=`&district_like=${l}`:e.target.type==="radio"&&e.target.classList.contains("priceRadio")?E=`&${e.target.dataset.price}&_sort=price&_order=desc`:e.target.type==="checkbox"&&e.target.classList.contains("houseCheckbox")?n+=`&type=${l}`:e.target.type==="checkbox"&&e.target.classList.contains("lifeEquipmentCheckbox")?n+=`&lifeEquipment_like=${l}`:e.target.type==="checkbox"&&e.target.classList.contains("equipmentCheckbox")?n+=`&equipment_like=${l}`:e.target.type==="checkbox"&&e.target.classList.contains("identityCheckbox")?n+=`&identity_like=${l}`:e.target.type==="checkbox"&&e.target.classList.contains("genderCheckbox")?n+=`&gender=${l}`:e.target.type==="checkbox"&&e.target.classList.contains("canPetCookCheckbox")&&(l=="可開伙"?n+=`&canCooking=${!0}`:n+=`&canPet=${!0}`),t=n+E+"&status=刊登中",d.get(`${f}rents?${t}`).then(function(u){u.data.length>0?(a=u.data,$(t,1,h),r=1,m(a)):(a=u.data,_(),r=1,m(a))})):(e.target.type==="radio"&&e.target.classList.contains("cityRadio")?n=n.replace(`address_like=${l}`,""):e.target.type==="checkbox"&&e.target.classList.contains("districtCheckbox")?n=n.replace(`&district_like=${l}`,""):e.target.type==="radio"&&e.target.classList.contains("priceRadio")?E=n.replace(`&${e.target.dataset.price}`,""):e.target.type==="checkbox"&&e.target.classList.contains("houseCheckbox")?n=n.replace(`&type=${l}`,""):e.target.type==="checkbox"&&e.target.classList.contains("lifeEquipmentCheckbox")?n=n.replace(`&lifeEquipment_like=${l}`,""):e.target.type==="checkbox"&&e.target.classList.contains("equipmentCheckbox")?n=n.replace(`&equipment_like=${l}`,""):e.target.type==="checkbox"&&e.target.classList.contains("identityCheckbox")?n=n.replace(`&identity_like=${l}`,""):e.target.type==="checkbox"&&e.target.classList.contains("genderCheckbox")?n=n.replace(`&gender=${l}`,""):e.target.type==="checkbox"&&e.target.classList.contains("canPetCookCheckbox")&&(l=="可開伙"?n=n.replace(`&canCooking=${!0}`,""):n=n.replace(`&canPet=${!0}`,"")),t=n+E+"&status=刊登中",d.get(`${f}rents?${t}`).then(function(u){u.data.length>0?(a=u.data,$(t,1,h),r=1,m(a)):(a=u.data,_(),r=1,m(a))}))})});let ne=document.querySelector("#close");ne.addEventListener("click",function(s){location.reload()});function se(){document.querySelectorAll(".link-title").forEach(a=>{a.addEventListener("click",function(c){c.preventDefault();const i=new URL(c.target.href),b=new URLSearchParams(i.search).get("id");d.get(`${f}rents/${b}`).then(function(p){const v=p.data;let x={view:parseInt(v.view)+1};d.patch(`${f}rents/${b}`,x).then(function(q){window.location.href=c.target.href}).catch(function(q){console.error(q)})}).catch(function(p){console.error(p)})})})}function re(){document.querySelectorAll(".favorite").forEach(a=>{a.addEventListener("click",function(c){let i=parseInt(c.target.dataset.id),o=parseInt(localStorage.getItem("userId")),b={rentId:i,userId:o};o?d.get(`${f}users/${o}/favorites`).then(function(p){const v=p.data.find(x=>x.rentId===i);v&&S.fire({icon:"warning",title:"您已經有這則貼文",showConfirmButton:!1,timer:1500}),v===void 0&&d.post(`${f}favorites`,b).then(function(x){S.fire({icon:"success",title:"添加至您的收藏",showConfirmButton:!1,timer:1500})})}):S.fire({icon:"warning",title:"請先登入帳號",showConfirmButton:!1,timer:1500})})})}
--- assets/user_posts-dd7f3998.js
+++ assets/user_posts-dd7f3998.js
@@ -1,7 +1,7 @@
-import{a as d}from"./bootstrap.min-f14bc68a.js";import{S as r}from"./sweetalert2.all-9c23d2ec.js";const _="https://roomie-lfta.onrender.com",F=localStorage.getItem("userId"),U=localStorage.getItem("token"),Y={headers:{Authorization:`Bearer ${U}`}};let j;const $=document.querySelectorAll(".nav-logged"),v=document.querySelectorAll(".nav-unlogged"),P=document.querySelectorAll(".nav-logged-photo");(async()=>{try{const s=`/600/users/${F}`,e=`${_}${s}`,t=await d.get(e,Y),o=t.data.photo;P.forEach(a=>a.setAttribute("src",o)),j=t.data,E(),$.forEach(a=>{a.classList.remove("d-none")}),v.forEach(a=>{a.classList.add("d-none")}),console.log("已登入")}catch(s){console.log(s),$.forEach(e=>{e.classList.add("d-none")}),v.forEach(e=>{e.classList.remove("d-none")}),console.log("未登入")}})();function E(){localStorage.setItem("user",JSON.stringify(j))}const p="https://roomie-lfta.onrender.com",m=localStorage.getItem("userId"),H=localStorage.getItem("token"),c={headers:{Authorization:`Bearer ${H}`}};let w,x,L,k;const B=document.querySelector("#publishList"),T=document.querySelector("#matchedList"),D=document.querySelector("#removedList"),S=document.querySelector("#draftList"),z=document.querySelectorAll(".user-post-list"),R=document.querySelector("#postsTab");let C="publish-tab";function y(){const s=`${p}/600/rents?userId=${m}&status=刊登中`;d.get(s,c).then(e=>{w=e.data,J(w)}).catch(e=>console.log(e))}function J(s){if(!s.length){B.innerHTML=`<div class="d-flex justify-content-center align-items-center">
+import{a as d,S as r}from"./bootstrap.min-ee9b3e4d.js";const _="https://roomie-lfta.onrender.com",F=localStorage.getItem("userId"),U=localStorage.getItem("token"),Y={headers:{Authorization:`Bearer ${U}`}};let C;const v=document.querySelectorAll(".nav-logged"),$=document.querySelectorAll(".nav-unlogged"),P=document.querySelectorAll(".nav-logged-photo"),E=document.querySelector(".logout-btn");(async()=>{try{const s=`/600/users/${F}`,e=`${_}${s}`,t=await d.get(e,Y),a=t.data.photo;P.forEach(o=>o.setAttribute("src",a)),C=t.data,H(),v.forEach(o=>{o.classList.remove("d-none")}),$.forEach(o=>{o.classList.add("d-none")})}catch(s){console.log(s),v.forEach(e=>{e.classList.add("d-none")}),$.forEach(e=>{e.classList.remove("d-none")})}})();function H(){localStorage.setItem("user",JSON.stringify(C))}E.addEventListener("click",z);function z(){r.fire({icon:"question",title:"確定要登出嗎",showCancelButton:!0,confirmButtonText:"確定",cancelButtonText:"取消"}).then(s=>{s.isConfirmed&&(localStorage.removeItem("userId"),localStorage.removeItem("token"),localStorage.removeItem("user"),r.fire({icon:"success",title:"已登出",timer:1500,showConfirmButton:!1}),setTimeout(()=>{location.href="login.html"},1e3))})}const p="https://roomie-lfta.onrender.com",m=localStorage.getItem("userId"),R=localStorage.getItem("token"),c={headers:{Authorization:`Bearer ${R}`}};let w,x,L,k;const B=document.querySelector("#publishList"),T=document.querySelector("#matchedList"),S=document.querySelector("#removedList"),D=document.querySelector("#draftList"),J=document.querySelectorAll(".user-post-list"),N=document.querySelector("#postsTab");let j="publish-tab";function y(){const s=`${p}/600/rents?userId=${m}&status=刊登中`;d.get(s,c).then(e=>{w=e.data,O(w)}).catch(e=>console.log(e))}function O(s){if(!s.length){B.innerHTML=`<div class="d-flex justify-content-center align-items-center">
     <p>尚無相關物件 ¯_(ツ)_/¯</p>
-  </div>`;return}B.innerHTML=s.reduce((e,t)=>{const o=t.traffic.reduce((a,l)=>a+`<span class="me-3 px-1 bg-primary-200"
-      >${l}</span
+  </div>`;return}B.innerHTML=s.reduce((e,t)=>{const a=t.traffic.reduce((o,i)=>o+`<span class="me-3 px-1 bg-primary-200"
+      >${i}</span
     >`,"");return e+`<div class="user-post-item row p-1 rounded hover-primary-2" data-post-id=${t.id}>
     <div class="col-12 col-md-3 p-0 rounded-top rounded-md-start">
       <img
@@ -83,7 +83,7 @@ import{a as d}from"./bootstrap.min-f14bc68a.js";import{S as r}from"./sweetalert2
             style="transform: translateY(25%)"
             >map</span
           >
- ${o}
+ ${a}
         </li>
         <li class="pb-2 h2 text-secondary text-end">
           ${t.price}元/月
@@ -100,9 +100,9 @@ import{a as d}from"./bootstrap.min-f14bc68a.js";import{S as r}from"./sweetalert2
         </li>
       </ul>
     </div>
-  </div>`},"")}function q(){const s=`${p}/600/rents?userId=${m}&status=已媒合`;d.get(s,c).then(e=>{x=e.data,N(x)}).catch(e=>console.log(e))}function N(s){let e="";if(!s.length){T.innerHTML=`<div class="d-flex justify-content-center align-items-center">
+  </div>`},"")}function q(){const s=`${p}/600/rents?userId=${m}&status=已媒合`;d.get(s,c).then(e=>{x=e.data,V(x)}).catch(e=>console.log(e))}function V(s){let e="";if(!s.length){T.innerHTML=`<div class="d-flex justify-content-center align-items-center">
     <p>尚無相關物件 ¯_(ツ)_/¯</p>
-  </div>`;return}s.forEach(function(t){const o=`${t.updateDate}`,a=new Date(o),l=`${t.soldDate}`,n=new Date(l),i=new Date,f=a.getTime()-n.getTime(),b=n.getTime()-i.getTime(),h=Math.abs(Math.trunc(f/(1e3*3600*24))),M=Math.abs(Math.trunc(b/(1e3*3600*24)));e+=`<div class="user-post-item col-12 my-3 p-1 hover-primary-2 rounded">
+  </div>`;return}s.forEach(function(t){const a=`${t.updateDate}`,o=new Date(a),i=`${t.soldDate}`,n=new Date(i),l=new Date,f=o.getTime()-n.getTime(),b=n.getTime()-l.getTime(),h=Math.abs(Math.trunc(f/(1e3*3600*24))),M=Math.abs(Math.trunc(b/(1e3*3600*24)));e+=`<div class="user-post-item col-12 my-3 p-1 hover-primary-2 rounded">
                     <div class="col-12 p-3 bg-white d-flex flex-wrap justify-content-evenly align-items-center text-end text-lg-center border rounded">
                         <div class="col-12 col-lg-4"><a href="matchArticle.html?id=${t.id}" class="link-dark fw-bold">${t.title}</a></div>
                         <div class="col-12 col-lg pb-1 pb-lg-0"><span class="text-primary">${M}日前&nbsp;</span>媒合成功</div>
@@ -136,10 +136,10 @@ import{a as d}from"./bootstrap.min-f14bc68a.js";import{S as r}from"./sweetalert2
                       </div>
                     </div>
                 </div>
-                `}),T.innerHTML=e}function A(){const s=`${p}/600/rents?userId=${m}&status=下架`;d.get(s,c).then(e=>{L=e.data,O(L)}).catch(e=>console.log(e))}function O(s){if(!s.length){D.innerHTML=`<div class="d-flex justify-content-center align-items-center">
+                `}),T.innerHTML=e}function I(){const s=`${p}/600/rents?userId=${m}&status=下架`;d.get(s,c).then(e=>{L=e.data,W(L)}).catch(e=>console.log(e))}function W(s){if(!s.length){S.innerHTML=`<div class="d-flex justify-content-center align-items-center">
     <p>尚無相關物件 ¯_(ツ)_/¯</p>
-  </div>`;return}D.innerHTML=s.reduce((e,t)=>{const o=t.traffic.reduce((a,l)=>a+`<span class="me-3 px-1 bg-primary-200"
-      >${l}</span
+  </div>`;return}S.innerHTML=s.reduce((e,t)=>{const a=t.traffic.reduce((o,i)=>o+`<span class="me-3 px-1 bg-primary-200"
+      >${i}</span
     >`,"");return e+`<div class="user-post-item row p-1 rounded hover-primary-2" data-post-id=${t.id}>
     <div class="col-12 col-md-3 p-0 rounded-top rounded-md-start">
       <img
@@ -218,7 +218,7 @@ import{a as d}from"./bootstrap.min-f14bc68a.js";import{S as r}from"./sweetalert2
             style="transform: translateY(25%)"
             >map</span
           >
- ${o}
+ ${a}
         </li>
         <li class="pb-2 h2 text-secondary text-end">
           ${t.price}元/月
@@ -235,10 +235,10 @@ import{a as d}from"./bootstrap.min-f14bc68a.js";import{S as r}from"./sweetalert2
         </li>
       </ul>
     </div>
-  </div>`},"")}function I(){const s=`${p}/600/rents?userId=${m}&status=草稿`;d.get(s,c).then(e=>{k=e.data,V(k)}).catch(e=>console.log(e))}function V(s){if(!s.length){S.innerHTML=`<div class="d-flex justify-content-center align-items-center">
+  </div>`},"")}function A(){const s=`${p}/600/rents?userId=${m}&status=草稿`;d.get(s,c).then(e=>{k=e.data,G(k)}).catch(e=>console.log(e))}function G(s){if(!s.length){D.innerHTML=`<div class="d-flex justify-content-center align-items-center">
     <p>尚無相關物件 ¯_(ツ)_/¯</p>
-  </div>`;return}S.innerHTML=s.reduce((e,t)=>{const o=t.traffic.reduce((a,l)=>a+`<span class="me-3 px-1 bg-primary-200"
-      >${l}</span
+  </div>`;return}D.innerHTML=s.reduce((e,t)=>{const a=t.traffic.reduce((o,i)=>o+`<span class="me-3 px-1 bg-primary-200"
+      >${i}</span
     >`,"");return e+`<div class="user-post-item row p-1 rounded hover-primary-2" data-post-id=${t.id}>
     <div class="col-12 col-md-3 p-0 rounded-top rounded-md-start">
       <img
@@ -317,7 +317,7 @@ import{a as d}from"./bootstrap.min-f14bc68a.js";import{S as r}from"./sweetalert2
             style="transform: translateY(25%)"
             >map</span
           >
- ${o}
+ ${a}
         </li>
         <li class="pb-2 h2 text-secondary text-end">
           ${t.price}元/月
@@ -334,4 +334,4 @@ import{a as d}from"./bootstrap.min-f14bc68a.js";import{S as r}from"./sweetalert2
         </li>
       </ul>
     </div>
-  </div>`},"")}R.addEventListener("click",s=>{const e=s.target.getAttribute("id");switch(C=e,e){case"publish-tab":y();break;case"matched-tab":q();break;case"removed-tab":A();break;case"draft-tab":I();break}});z.forEach(s=>{s.addEventListener("click",e=>{const t=e.target.classList;if(t.contains("to-removed"))return g("下架",e);if(t.contains("to-matched"))return g("已媒合",e);if(t.contains("to-publish"))return g("刊登中",e);if(t.contains("delete-post"))return W(e)})});function g(s,e){const t=e.target.closest(".user-post-item").getAttribute("data-post-id"),o=`${p}/600/rents/${t}`,a={status:s};let l;if(s==="已媒合"){const n=new Date,i=n.getFullYear(),f=(n.getMonth()+1).toString().padStart(2,"0"),b=n.getDate().toString().padStart(2,"0"),h=`${i}/${f}/${b}`;l={status:s,soldDate:h}}switch(s){case"下架":r.fire({icon:"question",title:"確定要暫時下架嗎",text:"點選確定後將移至下架區，可以至下架區再次上架貼文",showCancelButton:!0,confirmButtonText:"確定",cancelButtonText:"取消"}).then(n=>{n.isConfirmed&&d.patch(o,a,c).then(i=>{u(),r.fire({icon:"success",title:"下架貼文成功",timer:1500,showConfirmButton:!1})}).catch(i=>{r.fire({icon:"error",title:"下架貼文失敗",timer:1500,showConfirmButton:!1}),console.log(i)})});break;case"已媒合":r.fire({icon:"question",title:"確定媒合成功嗎",text:"一旦點選確定後將移至已媒合，無法再次上架貼文",input:"checkbox",inputPlaceholder:"我同意將貼文保留在媒合成功頁面",inputValidator:n=>!n&&"請勾選同意",showCancelButton:!0,confirmButtonText:"確定",cancelButtonText:"取消"}).then(n=>{n.isConfirmed&&d.patch(o,l,c).then(i=>{u(),r.fire({icon:"success",title:"恭喜您媒合成功",timer:1500,showConfirmButton:!1})}).catch(i=>{r.fire({icon:"error",title:"發生錯誤",timer:1500,showConfirmButton:!1})})});break;case"刊登中":r.fire({icon:"question",title:"確定要上架貼文嗎",showCancelButton:!0,confirmButtonText:"確定",cancelButtonText:"取消"}).then(n=>{n.isConfirmed&&d.patch(o,a,c).then(i=>{u(),r.fire({icon:"success",title:"上架貼文成功",timer:1500,showConfirmButton:!1})}).catch(i=>{r.fire({icon:"error",title:"上架貼文失敗",timer:1500,showConfirmButton:!1})})});break}}function W(s){const e=s.target.closest(".user-post-item").getAttribute("data-post-id"),t=`${p}/600/rents/${e}`;r.fire({icon:"question",title:"確定要刪除貼文嗎",text:"一旦刪除後將無法復原",showCancelButton:!0,confirmButtonText:"確定",cancelButtonText:"取消"}).then(o=>{o.isConfirmed&&d.delete(t,c).then(a=>{u(),r.fire({icon:"success",title:"刪除貼文成功",timer:1500,showConfirmButton:!1})}).catch(a=>{r.fire({icon:"error",title:"刪除貼文失敗",timer:1500,showConfirmButton:!1})})})}function u(){switch(C){case"publish-tab":y();break;case"matched-tab":q();break;case"removed-tab":A();break;case"draft-tab":I();break}}function G(){y()}G();[].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+  </div>`},"")}N.addEventListener("click",s=>{const e=s.target.getAttribute("id");switch(j=e,e){case"publish-tab":y();break;case"matched-tab":q();break;case"removed-tab":I();break;case"draft-tab":A();break}});J.forEach(s=>{s.addEventListener("click",e=>{const t=e.target.classList;if(t.contains("to-removed"))return g("下架",e);if(t.contains("to-matched"))return g("已媒合",e);if(t.contains("to-publish"))return g("刊登中",e);if(t.contains("delete-post"))return K(e)})});function g(s,e){const t=e.target.closest(".user-post-item").getAttribute("data-post-id"),a=`${p}/600/rents/${t}`,o={status:s};let i;if(s==="已媒合"){const n=new Date,l=n.getFullYear(),f=(n.getMonth()+1).toString().padStart(2,"0"),b=n.getDate().toString().padStart(2,"0"),h=`${l}/${f}/${b}`;i={status:s,soldDate:h}}switch(s){case"下架":r.fire({icon:"question",title:"確定要暫時下架嗎",text:"點選確定後將移至下架區，可以至下架區再次上架貼文",showCancelButton:!0,confirmButtonText:"確定",cancelButtonText:"取消"}).then(n=>{n.isConfirmed&&d.patch(a,o,c).then(l=>{u(),r.fire({icon:"success",title:"下架貼文成功",timer:1500,showConfirmButton:!1})}).catch(l=>{r.fire({icon:"error",title:"下架貼文失敗",timer:1500,showConfirmButton:!1}),console.log(l)})});break;case"已媒合":r.fire({icon:"question",title:"確定媒合成功嗎",text:"一旦點選確定後將移至已媒合，無法再次上架貼文",input:"checkbox",inputPlaceholder:"我同意將貼文保留在媒合成功頁面",inputValidator:n=>!n&&"請勾選同意",showCancelButton:!0,confirmButtonText:"確定",cancelButtonText:"取消"}).then(n=>{n.isConfirmed&&d.patch(a,i,c).then(l=>{u(),r.fire({icon:"success",title:"恭喜您媒合成功",timer:1500,showConfirmButton:!1})}).catch(l=>{r.fire({icon:"error",title:"發生錯誤",timer:1500,showConfirmButton:!1})})});break;case"刊登中":r.fire({icon:"question",title:"確定要上架貼文嗎",showCancelButton:!0,confirmButtonText:"確定",cancelButtonText:"取消"}).then(n=>{n.isConfirmed&&d.patch(a,o,c).then(l=>{u(),r.fire({icon:"success",title:"上架貼文成功",timer:1500,showConfirmButton:!1})}).catch(l=>{r.fire({icon:"error",title:"上架貼文失敗",timer:1500,showConfirmButton:!1})})});break}}function K(s){const e=s.target.closest(".user-post-item").getAttribute("data-post-id"),t=`${p}/600/rents/${e}`;r.fire({icon:"question",title:"確定要刪除貼文嗎",text:"一旦刪除後將無法復原",showCancelButton:!0,confirmButtonText:"確定",cancelButtonText:"取消"}).then(a=>{a.isConfirmed&&d.delete(t,c).then(o=>{u(),r.fire({icon:"success",title:"刪除貼文成功",timer:1500,showConfirmButton:!1})}).catch(o=>{r.fire({icon:"error",title:"刪除貼文失敗",timer:1500,showConfirmButton:!1})})})}function u(){switch(j){case"publish-tab":y();break;case"matched-tab":q();break;case"removed-tab":I();break;case"draft-tab":A();break}}function Q(){y()}Q();[].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
